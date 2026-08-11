2c27fc97e29aa89b521a84dbd84fed5fcc64df91
lucasweber20 <lucasweber449@gmail.com>
2026-08-11 10:33:25 -0300

    Feat: add arguments flag

--- statusx.py
+++ statusx.py
@@ -1,7 +1,20 @@
+import argparse
 
+
+parser = argparse.ArgumentParser()
+
+args = parser.add_argument("-u", "--url", help='Specify url, example: -u https://example.com/?param=value', nargs="+", type=str)
+args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
+args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
+args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", type=int)
+
+args = parser.parse_args()
 
 def main():
-    pass
+    url = args.url
+    file = args.list
+    output = args.output
+    thread = args.thread
 
 if __name__ == "__main__":
     main()
