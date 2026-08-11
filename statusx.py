d1aff16daf2420eee7bf881934367353cfdc3e43
lucasweber20 <lucasweber449@gmail.com>
2026-08-11 10:43:17 -0300

    Feat: read file

--- statusx.py
+++ statusx.py
@@ -1,20 +1,39 @@
 import argparse
+import requests
+import concurrent.futures
 
 
 parser = argparse.ArgumentParser()
 
-args = parser.add_argument("-u", "--url", help='Specify url, example: -u https://example.com/?param=value', nargs="+", type=str)
 args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
 args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
 args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", type=int)
+args = parser.add_argument("-ti", "--timeout", help="Specify timeout number, example: --timeout 5", default=5, type=int)
 
 args = parser.parse_args()
 
+FILE = args.list
+OUTPUT = args.output
+THREAD = args.thread
+TIMEOUT = args.timeout
+
 def main():
-    url = args.url
-    file = args.list
-    output = args.output
-    thread = args.thread
+
+    # Read file
+    urls = []
+    with open(FILE, 'r') as f:
+        for file in f:
+            urls.append(file.strip())
+
+def requests_urls(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"}
+    try:
+        req = requests.get(url, headers=headers, allow_redirects=False, timeout=TIMEOUT)
+        req_url = req.url
+        status_code = req.status_code
+        return req_url, status_code
+    except:
+        pass
 
 if __name__ == "__main__":
     main()
